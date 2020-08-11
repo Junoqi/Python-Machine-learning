222b55507afc9f89b3d727107fc196fe3a9bf390
Henry Pope IV <51766577+Junoqi@users.noreply.github.com>
2020-08-11 13:26:20 -0400

    Update schoolmodel.py

--- schoolmodel.py
+++ schoolmodel.py
@@ -4,7 +4,7 @@ import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
-data = pd.read_csv('C:\\Users\\Henry Pope IV\\Desktop\\student-mat.csv', sep=";")
+data = pd.read_csv('student-mat.csv', sep=";")
 
 data = data[["G1", "G2", "G3", "studytime"]]
 
